df8f9dbe78dea046a560b467342a67c3be584cf8
unknown <1761212789@qq.com>
2022-10-09 11:00:45 +0800

    Site updated: 2022-10-09 11:00:44

---
+++ js/weather.js
@@ -0,0 +1,22 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "FFFFFF",
+    "tmpSize": "16",
+    "cityColor": "FFFFFF",
+    "citySize": "16",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "borderRadius": "5",
+    "fixed": "false",
+    "vertical": "top",
+    "horizontal": "left",
+    "key": "806a52ef4bb4457dbfe0813a58e00e30"
+  }
+}
